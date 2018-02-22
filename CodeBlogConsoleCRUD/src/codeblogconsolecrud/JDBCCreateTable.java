017c041c5e59e1b2863a189983f89d3ef425ad8d
Wwawa <wahiddah_longazizi@yahoo.com>
2018-02-22 16:53:15 +0800

    Example

---
+++ CodeBlogConsoleCRUD/src/codeblogconsolecrud/JDBCCreateTable.java
@@ -0,0 +1,61 @@
+package codeblogconsolecrud;
+//STEP 1. Import required packages
+import java.sql.*;
+
+public class JDBCCreateTable {
+   // JDBC driver name and database URL
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://localhost:3306/codeblog_java";
+
+   //  Database credentials
+   static final String USER = "wawa";
+   static final String PASS = "mypass!@#";
+   
+   public static void main(String[] args) {
+   Connection conn = null;
+   Statement stmt = null;
+   try{
+      //STEP 2: Register JDBC driver
+      Class.forName("com.mysql.jdbc.Driver");
+
+      //STEP 3: Open a connection
+      System.out.println("Connecting to a selected database...");
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      System.out.println("Connected database successfully...");
+      
+      //STEP 4: Execute a query
+      System.out.println("Creating table in given database...");
+      stmt = conn.createStatement();
+      
+      String sql = "CREATE TABLE codeblog_java.contacts " +
+               "(id int(11) NOT NULL AUTO_INCREMENT," +
+    		    "name varchar(45) DEFAULT NULL," +
+    		    "email varchar(45) DEFAULT NULL," +
+    		    "contact_number varchar(45) DEFAULT NULL," +
+    		    "PRIMARY KEY (`id`))"; 
+    		  
+      stmt.executeUpdate(sql);
+      System.out.println("Created table in given database...");
+   }catch(SQLException se){
+      //Handle errors for JDBC
+      se.printStackTrace();
+   }catch(Exception e){
+      //Handle errors for Class.forName
+      e.printStackTrace();
+   }finally{
+      //finally block used to close resources
+      try{
+         if(stmt!=null)
+            conn.close();
+      }catch(SQLException se){
+      }// do nothing
+      try{
+         if(conn!=null)
+            conn.close();
+      }catch(SQLException se){
+         se.printStackTrace();
+      }//end finally try
+   }//end try
+   System.out.println("Goodbye!");
+}//end main
+}//end JDBCExample
